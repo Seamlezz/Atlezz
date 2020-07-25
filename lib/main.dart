4ab4e2ca7878b433ebc4a8c11228bd04645b57dd
gabber235 <gabber200@icloud.com>
2020-07-25 12:51:22 +0200

    :construction_worker: Changed analyzing

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(AtlezzApp());
 }
+
+FirebaseAnalytics analytics = FirebaseAnalytics();
 
 class AtlezzApp extends StatelessWidget {
   @override
@@ -10,6 +14,9 @@ class AtlezzApp extends StatelessWidget {
     return MaterialApp(
       title: "Atlezz",
       home: Container(),
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: analytics),
+      ],
     );
   }
 }
