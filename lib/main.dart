ac00f1e0aa54bd290391878521c6ac12a797888f
gabber235 <gabber200@icloud.com>
2020-07-28 09:01:57 +0200

    :sparkles: Setup crashlytics for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  Crashlytics.instance.enableInDevMode = true;
+  // Pass all uncaught errors from the framework to Crashlytics.
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
   runApp(AtlezzApp());
 }
 
